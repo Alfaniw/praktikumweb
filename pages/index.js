84e68c9a0c3981b9250e0362a620b295178398de
alfaniw <alfaniw4@gmail.com>
2020-07-21 20:30:43 +0700

    publish folder

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Beranda(){
+    return (
+        <div>
+            <main>
+                <h1>Pertanian Shop</h1>
+                <p>Aplikasi ini memudahkan petani untuk mencari bibit tanaman dan memudahkan petani untuk mengetahui bagaimana merawat tanaman tersebut</p>    
+            </main> 
+            </div>
+    );
+}
+export default Beranda;
